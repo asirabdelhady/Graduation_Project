f4d03c65183dbc79b347d80b3f974798ef9071db
hobvvv <adhfjv7@gmail.com>
2022-04-27 04:08:27 +0200

    tour screen hobv

--- lib/modules/tour/tour_screen.dart
+++ lib/modules/tour/tour_screen.dart
@@ -14,8 +14,63 @@ class _TourScreenState extends State<TourScreen> {
   @override
   Widget build(BuildContext context) {
 
-    return const Scaffold(
-      body: Center(child: Text('Tour Screen')),
+    return  Scaffold(
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: Text.rich(TextSpan(text: 'Tour Plan', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30))),
+            ),
+            /* Expanded(
+          child: Stepper(
+            type: stepperType,
+            physics: ScrollPhysics(),
+            currentStep: _currentStep,
+            //onStepTapped: (step) => tapped(step),
+            //onStepContinue:  continued,
+            //onStepCancel: cancel,
+            steps: <Step>[
+              Step(
+                title: Text('The Great Pyramids of Giza'),
+                content: Column(
+                  children: [
+                    categoryCard(minWidth: 101, height: 37, sightname: 'Pyramids', onPressed: (){},)
+
+                  ]
+                ),
+                isActive: _currentStep >= 0,
+                state: _currentStep >= 0 ?
+               StepState.complete : StepState.disabled,
+              ),
+              Step(
+                title: new Text('The Nile River'),
+                content: Column(
+                  children: [
+                    categoryCard(minWidth: 101, height: 37, sightname: 'The Nile', onPressed: (){},)
+
+
+                  ]),
+                isActive: _currentStep >= 0,
+                state: _currentStep >= 1 ?
+                StepState.complete : StepState.disabled,
+              ),
+              Step(
+                title: new Text('Qasr El Baroon'),
+                content: Column(
+                  children: [
+                    categoryCard(minWidth: 101, height: 37, sightname: 'Qasr El Baroon', onPressed: (){},)
+
+                  ],
+                ),
+                isActive:_currentStep >= 0,
+                state: _currentStep >= 2 ?
+                StepState.complete : StepState.disabled,
+              ),
+            ],
+          ),
+        ),*/
+          ]),
 
     );
   }
