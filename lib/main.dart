63293292847da8f5fe0f88cf5c059eefa84779f1
Asir <asirabdelhady@gmail.com>
2022-05-04 15:34:07 +0200

    Fire base initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tour_guide_app/modules/categories/category_screen.dart';
 import 'package:tour_guide_app/modules/home/homescreen.dart';
@@ -10,7 +12,9 @@ import 'modules/signup/signuppage.dart';
 import 'modules/welcome/welcomepage.dart';
 
 
-void main(){
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
